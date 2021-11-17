d860149b23189ebe7427f2e6326a2770328fca04
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-17 09:29:14 -0300

    second case

--- pigLatin.js
+++ pigLatin.js
@@ -1,11 +1,24 @@
 function translatePigLatin(str) {
 const VOEWS = ['a','e','i','o','u']
 let result = ''
+let hasNoVows = true 
 const firstChar = str[0]
 if(VOEWS.includes(firstChar)){
     result =  `${str}way`
 }
+
+for (const char in str) {
+    if(VOEWS.includes(char)){
+        hasNoVows = false
+        break
+    }
+}
+
+if (hasNoVows) {
+    result = `${str}ay`
+}
+
     return result
 }
   
-console.log(translatePigLatin("algorithm"));
+console.log(translatePigLatin("glove"));
